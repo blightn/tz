b17f31440afb6fcb0c178ede76a6a14b9f3ebae8
blightn <blightan@gmail.com>
2022-04-10 12:16:45 +0300

    Update client's main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,10 +1,71 @@
 #include <iostream>
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
+#include <csignal>
+
+#include "Client.hpp"
+
+static Client* g_pClient = nullptr;
+
+void printUsage()
+{
+	std::cerr << "Usage:\n"
+	          << "\thost:port\n"
+	          << "\t--statistic"
+	          << std::endl;
+}
+
+void sigIntHandler(int signal)
+{
+	g_pClient->stop();
+}
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Client" << std::endl;
+	std::cout << "Client" << std::endl << std::endl;
 
-	return 0;
+	bool statistic = false;
+
+	if (argc == 2 || argc == 3 && (statistic = !strcmp(argv[2], "--statistic")))
+	{
+		try
+		{
+			std::string address(argv[1]);
+
+			std::vector<std::string> res;
+			boost::split(res, argv[1], [](char c) { return c == ':'; });
+
+			std::string host = res[0];
+			short port = boost::lexical_cast<short>(res[1]);
+
+			g_pClient = new Client(host, port);
+
+			if (statistic)
+			{
+				std::cout << "Statistics: " << g_pClient->getStatistics() << std::endl;
+			}
+			else
+			{
+				std::signal(SIGINT, &sigIntHandler);
+				g_pClient->start();
+			}
+
+			delete g_pClient;
+			g_pClient = nullptr;
+
+			return EXIT_SUCCESS;
+		}
+		catch (boost::bad_lexical_cast const&)
+		{
+			std::cout << "Bad port" << std::endl;
+		}
+		catch (std::exception const& ex)
+		{
+			std::cout << "Exception: " << ex.what() << std::endl;
+		}
+	}
+	else
+		printUsage();
+
+	return EXIT_FAILURE;
 }
